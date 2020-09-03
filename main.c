6b1c85c71aa802e03542ae4aad0222a21d5d0c94
samzolin <saz5193@psu.edu>
2020-09-03 17:11:05 +0000

    added line

--- main.c
+++ main.c
@@ -6,8 +6,11 @@ int main()
     float celsius, fahrenheit;
 
     printf("Enter temperature in celsius: ");
+
     scanf("%f", &celsius);
+
     fahrenheit = (celsius * 9 / 5) + 32;
+    
     printf("%f° Celsius is equivalent to %f° Fahrenheit.", celsius, fahrenheit);
    
 
